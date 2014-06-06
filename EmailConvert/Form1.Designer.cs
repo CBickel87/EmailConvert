3cd95c8bd6c6088246c8f8b1f21a9fda5d362460
Charles Bickel <CBickel87@gmail.com>
2014-06-06 14:44:56 -0400

    Working with Listbox

--- EmailConvert/Form1.Designer.cs
+++ EmailConvert/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -39,14 +39,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(260, 20);
             this.textBox1.TabIndex = 0;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 38);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(260, 342);
-            this.listBox1.TabIndex = 1;
             // 
             // button1
             // 
@@ -57,6 +49,14 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 342);
+            this.listBox1.TabIndex = 1;
             // 
             // Form1
             // 
@@ -76,8 +76,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- EmailConvert/Form1.cs
+++ EmailConvert/Form1.cs
@@ -24,21 +24,31 @@ namespace EmailConvert
             char[] delimiterChars = { ';'};
             string text = textBox1.Text;
             string[] words = text.Split(delimiterChars);
-
-            foreach (string s in words);
-
+           
+            foreach (string s in words)
+            {
+                string s1 = s.Trim();
+                //listBox1.Items.Add(string.Join(Environment.NewLine, s));
+                var names = s1.Split(',');
+                string lastname = names[0];
+                string firstname = names[1];
+                //listBox1.Items.Add(string.Join(Environment.NewLine, names));
+                listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
+            }
+            //{
             //string fullname = textBox1.Text;
             //var newname = fullname.Split(';');
             //i = newname[0++];
 
             //listBox1.Items.Add(string.Join(Environment.NewLine, newname[3]));
 
-            string fullname = s;
-            var names = fullname.Split(',');
-            string lastname = names[0];
-            string firstname = names[1];
+            //string fullname = (s);
+            //var names = s.Split(',');
+            //string lastname = names[0];
+            //string firstname = names[1];
 
-            listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
+            //listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
+           // }
         }
        
     }
