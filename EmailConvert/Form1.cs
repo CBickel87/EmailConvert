cef3a09cf27d4a939d60756d05473f771e0023dc
Charles Bickel <CBickel87@gmail.com>
2014-06-09 16:31:41 -0400

    Added ability to query domain
    Added some more reliability by querying domain with the user first and
    last name.

--- EmailConvert/Form1.cs
+++ EmailConvert/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace EmailConvert
 {
@@ -18,7 +20,7 @@ namespace EmailConvert
             InitializeComponent();
         }
 
-   
+
         private void button1_Click(object sender, EventArgs e)
         {
             char[] delimiterChars = { ';'};
@@ -28,15 +30,39 @@ namespace EmailConvert
             foreach (string s in words)
             {
                 string s1 = s.Trim();
-                //listBox1.Items.Add(string.Join(Environment.NewLine, s));
+                //listBox1.Items.Add(string.Join(Environment.NewLine, s)); // add item to listbox
                 var names = s1.Split(',');
                 string lastname = names[0];
                 string firstname = names[1];
-                textBox2.Text = (firstname + "." + lastname + "@email.com");
+                //textBox2.Text = (firstname + "." + lastname + "@email.com");
+
+                // create your domain context
+                PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+
+                // define a "query-by-example" principal - here, we search for a UserPrincipal 
+                // and with the first name (GivenName) of "Bruce" and a last name (Surname) of "Miller"
+                UserPrincipal qbeUser = new UserPrincipal(ctx);
+                qbeUser.GivenName = names[1];
+                qbeUser.Surname = names[0];
+
+                // create your principal searcher passing in the QBE principal    
+                PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+                // find all matches
+                foreach (var found in srch.FindAll())
+                {
+                    // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....
+                    //MessageBox.Show(found.UserPrincipalName);
+
+                    textBox2.Text = found.UserPrincipalName;
+
+                }
 
                 //listBox1.Items.Add(string.Join(Environment.NewLine, names));
                 //listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
             }
+
+            
             //{
             //string fullname = textBox1.Text;
             //var newname = fullname.Split(';');
