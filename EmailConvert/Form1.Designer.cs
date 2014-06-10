8228c4521323750bd9c74eb71ce04e9e9b0ca970
CBickel87 <CBickel87@gmail.com>
2014-06-10 02:18:29 -0400

    Cleaned up some code and fixed some stuff
    Re-uploaded all files since I didn't clone the project correctly when
    trying to work on it from home.
    
    Cleaned up most of the code.
    Textbox2 now adding new lines correctly.
    Added button to clear the textbox.
    Textbox also clears each time button1_Click event takes place.

--- EmailConvert/Form1.Designer.cs
+++ EmailConvert/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -42,11 +43,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 410);
+            this.button1.Location = new System.Drawing.Point(178, 410);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Enter";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -58,14 +59,27 @@
             this.textBox2.Size = new System.Drawing.Size(259, 365);
             this.textBox2.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 445);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Convert2Email";
             this.ResumeLayout(false);
@@ -78,6 +92,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- EmailConvert/Form1.cs
+++ EmailConvert/Form1.cs
@@ -20,64 +20,59 @@ namespace EmailConvert
             InitializeComponent();
         }
 
-
         private void button1_Click(object sender, EventArgs e)
         {
-            char[] delimiterChars = { ';'};
-            string text = textBox1.Text;
-            string[] words = text.Split(delimiterChars);
-           
-            foreach (string s in words)
-            {
-                string s1 = s.Trim();
-                //listBox1.Items.Add(string.Join(Environment.NewLine, s)); // add item to listbox
-                var names = s1.Split(',');
-                string lastname = names[0];
-                string firstname = names[1];
-                //textBox2.Text = (firstname + "." + lastname + "@email.com");
+            textBox2.Clear();
+            char[] delimiterChars = new Char[] {';'};
+            string[] words = textBox1.Text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
 
-                // create your domain context
-                PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
-
-                // define a "query-by-example" principal - here, we search for a UserPrincipal 
-                // and with the first name (GivenName) of "Bruce" and a last name (Surname) of "Miller"
-                UserPrincipal qbeUser = new UserPrincipal(ctx);
-                qbeUser.GivenName = names[1];
-                qbeUser.Surname = names[0];
-
-                // create your principal searcher passing in the QBE principal    
-                PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
-
-                // find all matches
-                foreach (var found in srch.FindAll())
+                foreach (string s in words)
                 {
-                    // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....
-                    //MessageBox.Show(found.UserPrincipalName);
-                    //textBox2.Text = found.UserPrincipalName;
-                    //Clipboard.SetText(found.UserPrincipalName);
+                    var names = s.Trim().Split(',');
+                    string lastname = names[0];
+                    string firstname = names[1];
+                    textBox2.Text += (firstname + "." + lastname + "@email.com" + "\r\n");
+                    //MessageBox.Show(firstname + "." + lastname + "@email.com");
 
+                    //// create your domain context
+                    //PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+
+                    //// define a "query-by-example" principal - here, we search for a UserPrincipal 
+                    //// and with the first name (GivenName) of "Bruce" and a last name (Surname) of "Miller"
+                    //UserPrincipal qbeUser = new UserPrincipal(ctx);
+                    //qbeUser.GivenName = names[1];
+                    //qbeUser.Surname = names[0];
+
+                    //// create your principal searcher passing in the QBE principal    
+                    //PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+                    //// find all matches
+                    //foreach (var found in srch.FindAll())
+                    //{
+                    //    // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....
+                    //    //MessageBox.Show(found.UserPrincipalName);
+                    //    textBox2.Text += found.UserPrincipalName + "\r\n";
+                    //    //Clipboard.SetText += (found.UserPrincipalName);
+
+                    //}
                 }
+                
+           }
 
-                //listBox1.Items.Add(string.Join(Environment.NewLine, names));
-                //listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
-            }
+        ////Trying to add Ctr+A shortcut to select all. Defaults off for multiline txtboxes
+        //private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Control && e.KeyCode == Keys.A)
+        //    {
+        //        textBox2.SelectAll();
+        //    }
+        //}
 
-            
-            //{
-            //string fullname = textBox1.Text;
-            //var newname = fullname.Split(';');
-            //i = newname[0++];
-
-            //listBox1.Items.Add(string.Join(Environment.NewLine, newname[3]));
-
-            //string fullname = (s);
-            //var names = s.Split(',');
-            //string lastname = names[0];
-            //string firstname = names[1];
-
-            //listBox1.Items.Add(string.Join(Environment.NewLine, firstname + "." + lastname + "@email.com"));
-           // }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
         }
-       
+
     }
+       
 }
