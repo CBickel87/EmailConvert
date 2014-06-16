c7558558cc19c0453d708b4997fe22685ef92d54
Charles Bickel <CBickel87@gmail.com>
2014-06-16 17:20:43 -0400

    Redid somethings
    Had some issues trying to implement LDAP syntax to query correctly.
    
    The Domain I was querying didn't have Exchange on it. Do not have access
    on exchange domain to query I believe... Which causes an issue with
    regular users being able to query to get SMTP field.
    
    Removed richTextbox and replaced it with regular textboxes since
    copytoclipboard works better with regular textbox.
    
    Added textBox3 to collect the failed email address queries.

--- EmailConvert/Form1.Designer.cs
+++ EmailConvert/Form1.Designer.cs
@@ -31,19 +31,22 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(15, 23);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.Size = new System.Drawing.Size(502, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(178, 410);
+            this.button1.Location = new System.Drawing.Point(278, 410);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -53,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(33, 410);
+            this.button2.Location = new System.Drawing.Point(175, 410);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -61,21 +64,50 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // richTextBox1
+            // textBox2
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(260, 366);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
+            this.textBox2.Location = new System.Drawing.Point(13, 86);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 318);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(278, 86);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 318);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Correct addresses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Failed addresses";
             // 
             // Form1
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 445);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(529, 445);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -92,7 +124,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- EmailConvert/Form1.cs
+++ EmailConvert/Form1.cs
@@ -22,19 +22,20 @@ namespace EmailConvert
 
         private void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Clear();
-            char[] delimiterChars = new Char[] {';'};
+            textBox2.Clear();
+            textBox3.Clear();
+            char[] delimiterChars = new Char[] { ';' };
             string[] words = textBox1.Text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
 
-                foreach (string s in words)
-                {
-                    var names = s.Trim().Split(',');
-                    string lastname = names[0];
-                    string firstname = names[1];
+            foreach (string s in words)
+            {
+                var names = s.Trim().Split(',');
+                string lastname = names[0];
+                string firstname = names[1];
 
-                    //If user has middle initial in name layout. It removes it to prevent mismatch
-                    if (firstname.Contains('.'))
-                        firstname = firstname.Substring(0, firstname.LastIndexOf(' '));
+                //If user has middle initial in name layout. It removes it to prevent mismatch
+                if (firstname.Contains('.'))
+                    firstname = firstname.Substring(0, firstname.LastIndexOf(' '));
 
                     ////This was for testing without being on the AD.
                     //richTextBox1.Text += (firstname + "." + lastname + "@email.com" + "\r\n");
@@ -44,29 +45,37 @@ namespace EmailConvert
                     /// The code for querying AD was taken from marc_s @:
                     /// http://stackoverflow.com/questions/9603878/how-can-i-search-users-in-active-directory-based-on-name-and-first-name
                     /// </summary>
-                     
-                    // create your domain context
-                    PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
 
-                    // define a "query-by-example" principal - here, we search for a UserPrincipal 
-                    // and with the first name (GivenName) of "Bruce" and a last name (Surname) of "Miller"
-                    UserPrincipal qbeUser = new UserPrincipal(ctx);
-                    qbeUser.GivenName = firstname;
-                    qbeUser.Surname = lastname;
 
-                    // create your principal searcher passing in the QBE principal    
-                    PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+                // create your domain context
+                PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
 
-                    // find all matches
-                    foreach (var found in srch.FindAll())
+                // define a "query-by-example" principal - here, we search for a UserPrincipal 
+                // and with the first name (GivenName) of "Bruce" and a last name (Surname) of "Miller"
+                UserPrincipal qbeUser = new UserPrincipal(ctx);
+                qbeUser.GivenName = firstname;
+                qbeUser.Surname = lastname;
+
+                // create your principal searcher passing in the QBE principal    
+                PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+                // find all matches
+                foreach (var found in srch.FindAll())
+                {
+                    // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....
+                    //Prints to richTextbox1 and copies it all to clipboard at the same time
+                    if (found.UserPrincipalName.Contains(" "))
                     {
-                        // do whatever here - "found" is of type "Principal" - it could be user, group, computer.....
-                        //Prints to richTextbox1 and copies it all to clipboard at the same time
-                        richTextBox1.Text += found.UserPrincipalName + "\r\n";
-                        
-                        //Not working right now.
-                        //Clipboard.SetText(richTextBox1.Text, TextDataFormat.Rtf);
+                        textBox3.Text += found.UserPrincipalName + "\r\n";
                     }
+                    else
+                    {
+                        textBox2.Text += found.UserPrincipalName + "\r\n";
+                    }
+
+                   Clipboard.SetText(textBox2.Text);
+                }
+
                 }
                 
            }
@@ -74,7 +83,8 @@ namespace EmailConvert
         //Click clear button and clears richtextbox1
         private void button2_Click(object sender, EventArgs e)
         {
-            richTextBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
         }
 
     }
